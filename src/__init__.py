999d903a5a83dcaea777f2a403b7984453c630df
naveen-astra <nbknight23@gmail.com>
2026-01-17 03:55:40 +0530

    refactor: update author name to Naveen Babu in source files

--- src/__init__.py
+++ src/__init__.py
@@ -14,27 +14,6 @@ __version__ = "1.0.0"
 __author__ = "Naveen Astra"
 __project__ = "Intelligent FOREX Exchange Rate Forecasting using Hybrid GARCH–LSTM and Big Data Analytics"
 
-# Import key components for easy access
-from .utils.config import *
-from .data.fetch_data import ForexDataFetcher
-from .data.preprocess import (
-    compute_log_returns,
-    compute_technical_indicators,
-    preprocess_data,
-    split_data
-)
-
-__all__ = [
-    'ForexDataFetcher',
-    'compute_log_returns',
-    'compute_technical_indicators',
-    'preprocess_data',
-    'split_data',
-    'DATA_CONFIG',
-    'GARCH_CONFIG',
-    'LSTM_CONFIG',
-    'HYBRID_CONFIG',
-    'TRAINING_CONFIG',
-    'PATHS',
-]
+# Keep imports minimal to avoid circular dependencies
+__all__ = ['__version__', '__author__', '__project__']
 __description__ = 'Intelligent FOREX Exchange Rate Forecasting using Hybrid GARCH-LSTM'
